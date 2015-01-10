03dbd3f906aac47f496daf18a6539471463e2ce9
Shahar Zimmerman <szimmer1@ucsc.edu>
2015-01-10 13:48:32 -0800

    add snapshot

--- client/views/dropzone/dropzone.js
+++ client/views/dropzone/dropzone.js
@@ -3,11 +3,19 @@ Template.dropzone.events({
   'dropped #dropzone': function(event, temp) {
     console.log('files dropped');
     FS.Utility.eachFile(event, function(file) {
-      console.log("accessing Images collection");
       var obj = Images.insert(file, function (err, fileObj) {
         //If !err, we have inserted new doc with ID fileObj._id, and
         //kicked off the data upload using HTTP
-        console.log(err);
+        if (!err) {
+          console.log("ALERT! added snapshot with eventId ,1");
+          Snapshots.insert({
+            eventId: ',1',
+            imageId: fileObj._id
+          });
+        }
+        else {
+          console.log(err);
+        }
       });
     });
   }
