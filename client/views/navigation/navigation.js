461fa54d9f6d3de5cdf892aa3752238af9917863
Shahar Zimmerman <szimmer1@ucsc.edu>
2015-01-10 23:00:37 -0800

    add event,get top rated image for event

--- client/views/navigation/navigation.js
+++ client/views/navigation/navigation.js
@@ -2,6 +2,23 @@ Template.navigation.events({
   'click #add-event' : function(event, t) {
     event.preventDefault();
     console.log('Add event logic here');
+    $('#addEventModal').modal('toggle');
+  },
+
+  'submit #add-event-form' : function (event, t) {
+    event.preventDefault();
+
+    var eventName = $('#event-name').val();
+    var eventDescription = $('#event-description').val();
+    var userId = Meteor.userId();
+
+    var eventId = Events.insert({
+      name : eventName,
+      description : eventDescription,
+      creatingUserId : userId
+    });
+
+    console.log(eventId);
   },
 
   'click #logout': function(e, t) {
