f559516dd41acb80d01af34665b0fdd65476c963
Shahar Zimmerman <szimmer1@ucsc.edu>
2015-01-11 11:24:00 -0800

    added text support

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -28,7 +28,14 @@ Template.dashboard.helpers({
     },
     showDropzone: function() {
         return Session.get("showDropzone") ? true : false;
+    },
+    isImg: function() {
+        return (this.type === 'image' || this.type === undefined)
+    },
+    isText: function() {
+        return (this.type === 'text')
     }
+
 });
 
 
@@ -37,6 +44,22 @@ Template.dashboard.events({
     'click div.event-text': function(event, t) {
         $('div.event-text').hide();
         $('input.event-text').show();
+    },
+
+    'submit .event-text-form': function(e,t) {
+        var eventText = $('#event-text').val();
+        if (eventText.length <= 100) {
+            var snapshotId = Snapshots.insert({
+                eventId: Session.get('eventId'),
+                type: 'text',
+                description: eventText,
+                createdAt: new Date()
+            });
+            console.log(snapshotId);
+        }
+        else {
+            alert('Write a shorter quote!')
+        }
     },
 
   'click .upvote' : function(e, t) {
